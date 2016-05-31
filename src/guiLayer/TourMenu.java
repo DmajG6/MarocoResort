3f02a5963e0bd194ab6d142971fad5449b28b460
Simeon Kolev <simeonvkolev@gmail.com>
2016-05-31 22:30:06 +0200

    Update TourMenu.java

--- src/guiLayer/TourMenu.java
+++ src/guiLayer/TourMenu.java
@@ -7,19 +7,44 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import modelLayer.Customer;
+import controlLayer.CustomerController;
 
-/**
-	May 30, 2016 - 1:14:39 PM
-*/
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+import controlLayer.*;
+import modelLayer.*;
+
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.awt.Choice;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Button;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 public class TourMenu extends JFrame {
-
+	
+	private ActivityController actCtr = new ActivityController();
+	
+	private Staff loggedInStaff;
 	private JPanel contentPane;
-	private Customer customer;
-	private int type;
-
-
+	private JTextField dateTXT;
+	private String date = null;
+	private JTable table;
+	private DefaultTableModel model;
+	private JTextField durationTXT;
+	private JTextField priceTxt;
 	/**
 	 * Launch the application.
 	 */
@@ -27,7 +52,7 @@ public class TourMenu extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					TourMenu frame = new TourMenu(new Customer(), 1);
+					TourMenu frame = new TourMenu(new Staff());
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -39,15 +64,101 @@ public class TourMenu extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public TourMenu(Customer customer, int type) {
-		this.type = type;
-		this.customer = customer;
+	public TourMenu(Staff staff) {
+		this.loggedInStaff = staff;
+		setTitle("Tour");
+		loggedInStaff = staff;
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1017, 643);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
+		
+		JButton btnSelect = new JButton("Select\r\n");
+		btnSelect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				getDate();
+			}
+		});
+		btnSelect.setBounds(276, 44, 125, 25);
+		contentPane.add(btnSelect);
+		
+		dateTXT = new JTextField();
+		dateTXT.setEditable(false);
+		dateTXT.setBounds(150, 45, 116, 23);
+		contentPane.add(dateTXT);
+		dateTXT.setColumns(10);
+			
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(168, 110, 773, 400);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Destination", "Time", "Price", "Yes/No"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class, double.class, String.class,
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		scrollPane.setViewportView(table);
+		
+		JButton btnBook = new JButton("Book");
+		btnBook.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				bookPressed();
+			}
+		});
+		btnBook.setBounds(844, 533, 97, 25);
+		contentPane.add(btnBook);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				exitPressed();
+			}
+		});
+		btnExit.setBounds(844, 558, 97, 25);
+		contentPane.add(btnExit);
+		
+		JLabel lblSelectTime = new JLabel("Select Time");
+		lblSelectTime.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblSelectTime.setBounds(74, 47, 85, 14);
+		contentPane.add(lblSelectTime);
+		
 
+		JLabel lblDuration = new JLabel("Duration");
+		lblDuration.setBounds(27, 205, 116, 16);
+		contentPane.add(lblDuration);
+		
+		durationTXT = new JTextField();
+		durationTXT.setEditable(false);
+		durationTXT.setBounds(27, 234, 116, 22);
+		contentPane.add(durationTXT);
+		durationTXT.setColumns(10);
+		
+		JLabel lblPrice = new JLabel("Price:");
+		lblPrice.setBounds(27, 269, 56, 16);
+		contentPane.add(lblPrice);
+		
+		priceTxt = new JTextField();
+		priceTxt.setEditable(false);
+		priceTxt.setBounds(27, 298, 116, 22);
+		contentPane.add(priceTxt);
+		priceTxt.setColumns(10);
+		
+		model = (DefaultTableModel) table.getModel();
+	}
+	
 }
+
+
